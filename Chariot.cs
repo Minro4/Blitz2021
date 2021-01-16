05f4cc153c6fe72ee11ed473c2e57a6b6af42921
chameau5050 <54971185+chameau5050@users.noreply.github.com>
2021-01-15 23:06:04 -0500

    patate

--- Chariot.cs
+++ Chariot.cs
@@ -12,37 +12,72 @@ namespace Blitz2020
 	public class Chariot
 	{
 		public bool done;
-		public Chariot() {
-			done = true;
+		public string id;
+		public State state;
+		public Position target;
+
+		public enum State
+		{
+			TRAVEL, PICKUP, RETURN, DROP,WAITTING
 		}
 
-		public UnitAction goTo(Unit kart, Position target){
+		public UnitAction selectAction(Unit a) 
+		{
+			if (state == State.TRAVEL)
+			{
+				if (target.Equals(a.position))
+				{
+					state = State.PICKUP;
+					return new UnitAction(UnitActionType.MOVE, id, target);
+				}
+				else 
+				{ 
+				
+				}
+					
+
+				return new UnitAction(UnitActionType.MOVE, id, target);
+			}
+			else if (state == State.TRAVEL) 
+			{ 
+			
+			}
+
+			return new UnitAction(UnitActionType.MOVE, id, target);
+		}
+
+		public Chariot(string id) {
+			done = true;
+			this.id = id;
+		}
+
+		public static UnitAction goTo(Unit kart, Position target){
 			UnitAction action;
 			if (estPerimetre(kart.position, target)) {
 				action = new UnitAction(UnitActionType.PICKUP, kart.id, target);
-				done = true;
+				//done = true;
 			}
 			else {
 				action = new UnitAction(UnitActionType.MOVE, kart.id, target);
-				done = false;
+				//done = false;
 			}
 			return action;
 		}
-		public UnitAction goToBase(Unit kart, Position target) {
+		public static UnitAction goToBase(Unit kart, Position target) {
 			UnitAction action;
 			if (estPerimetre(kart.position, target))
 			{
 				action = new UnitAction(UnitActionType.DROP, kart.id, target);
-				done = true;
+				//done = true;
 			}
 			else
 			{
 				action = new UnitAction(UnitActionType.MOVE, kart.id, target);
-				done = false;
+				//done = false;
 			}
 			return action;
 		}
-		public bool estPerimetre(Position kartPosition, Position target) {
+		public static bool estPerimetre(Position kartPosition, Position target) {
 			bool answer=false;
 			int distanceX =Math.Abs( kartPosition.x - target.x);
 			int distanceY = Math.Abs(kartPosition.y - target.y);
