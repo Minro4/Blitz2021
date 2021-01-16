6b79b42ab5ff7d601a04277d849e5ead0792d178
Charles <charlybelan@gmail.com>
2021-01-15 19:52:33 -0500

    création de la classe Chariot

---
+++ Chariot.cs
@@ -0,0 +1,54 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Blitz2021;
+
+namespace Blitz2020
+{
+
+	public class Chariot
+	{
+		public bool done;
+		public Chariot() {
+			done = true;
+		}
+
+		public UnitAction goTo(Unit kart, Position target){
+			UnitAction action;
+			if (estPerimetre(kart.position, target)) {
+				action = new UnitAction(UnitActionType.PICKUP, kart.id, target);
+				done = true;
+			}
+			else {
+				action = new UnitAction(UnitActionType.MOVE, kart.id, target);
+				done = false;
+			}
+			return action;
+		}
+		public UnitAction goToBase() {
+			UnitAction action;
+			if (estPerimetre(kart.position, target))
+			{
+				action = new UnitAction(UnitActionType.DROP, kart.id, target);
+				done = true;
+			}
+			else
+			{
+				action = new UnitAction(UnitActionType.MOVE, kart.id, target);
+				done = false;
+			}
+			return action;
+		}
+		public estPerimetre(Position kartPosition, Position target) {
+			bool answer=false;
+			int distanceX =Math.Abs( kartPosition.x - target.x);
+			int distanceY = Math.Abs(kartPosition.y - target.y);
+			if (distanceX <= 1 && distanceY <= 1) {
+				answer == true;
+			}
+
+			return answer;
+		}
+	}
+
+}
